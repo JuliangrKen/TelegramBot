cb19cdc76f9cba8c20a4e3147cb7950a153d6721
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-17 16:22:22 +0300

    Добавил InlineKeyboardController

---
+++ TelegramBot.ConsoleApp/Controllers/InlineKeyboardController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot.ConsoleApp.Controllers
+{
+    public class InlineKeyboardController
+    {
+        protected ITelegramBotClient telegramBotClient;
+
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient)
+        {
+            this.telegramBotClient = telegramBotClient;
+        }
+
+        public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
+        {
+            Console.WriteLine($"Контроллер {GetType().Name} обнаружил нажатие на кнопку");
+
+            if (callbackQuery == null) return;
+            
+            await telegramBotClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку", cancellationToken: ct);
+        }
+    }
+}
