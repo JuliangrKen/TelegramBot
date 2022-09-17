bf5a764dca828905f7984df6d40f5be92b95d03c
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-17 16:12:28 +0300

    Добавил VoiceMessageController

---
+++ TelegramBot.ConsoleApp/Controllers/VoiceMessageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace TelegramBot.ConsoleApp.Controllers
+{
+    public class VoiceMessageController : AbstractController
+    {
+        public VoiceMessageController(ITelegramBotClient telegramBotClient) : base(telegramBotClient)
+        {
+        }
+
+        public override async Task Hangle(Message message, CancellationToken ct)
+        {
+            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+            await telegramBotClient.SendTextMessageAsync(message.Chat.Id, $"Получено голосовое сообщение", cancellationToken: ct);
+        }
+    }
+}
