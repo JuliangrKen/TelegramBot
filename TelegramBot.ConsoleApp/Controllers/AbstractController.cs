35a813c6e740749e6edb1ce320b551c711b8a58a
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-17 15:54:08 +0300

    Добавил абстрактный класс контроллера

---
+++ TelegramBot.ConsoleApp/Controllers/AbstractController.cs
@@ -0,0 +1,30 @@
+﻿using Telegram.Bot;
+
+namespace TelegramBot.ConsoleApp.Controllers
+{
+    /// <summary>
+    /// Абстрактный класс для создания контроллеров
+    /// </summary>
+    public abstract class AbstractController
+    {
+        /// <summary>
+        /// Поле, хранящее ссылку на клиент бота телеграм
+        /// </summary>
+        protected readonly ITelegramBotClient telegramBotClient;
+
+        /// <summary>
+        /// Конструктор, инициализирующий telegramBotClient
+        /// </summary>
+        /// <param name="telegramBotClient"></param>
+        public AbstractController(ITelegramBotClient telegramBotClient)
+        {
+            this.telegramBotClient = telegramBotClient;
+        }
+
+        /// <summary>
+        /// Обязательный обработчик
+        /// </summary>
+        /// <returns></returns>
+        public abstract Task Hangle();
+    }
+}
