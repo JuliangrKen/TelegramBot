e95e9b77b0cefac859c46d647160201348403ad9
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-17 16:09:57 +0300

    Добавил TextMessageController с реализованым Hangle и конструктором

---
+++ TelegramBot.ConsoleApp/Controllers/TextMessageController.cs
@@ -0,0 +1,18 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot.ConsoleApp.Controllers
+{
+    public class TextMessageController : AbstractController
+    {
+        public TextMessageController(ITelegramBotClient telegramBotClient) : base(telegramBotClient)
+        {
+        }
+
+        public override async Task Hangle(Message message, CancellationToken ct)
+        {
+            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+            await telegramBotClient.SendTextMessageAsync(message.Chat.Id, $"Получено текстовое сообщение", cancellationToken: ct);
+        }
+    }
+}
