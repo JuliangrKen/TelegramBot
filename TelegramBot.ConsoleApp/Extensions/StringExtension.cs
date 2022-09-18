09c9f1b2f4d1322834201584ecd0e258c3f4e962
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-18 14:58:21 +0300

    Добавил метод раширения для строк

---
+++ TelegramBot.ConsoleApp/Extensions/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace TelegramBot.ConsoleApp.Extensions
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Преобразуем строку, чтобы она начиналась с заглавной буквы
+        /// </summary>
+        public static string UppercaseFirst(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return string.Empty;
+
+            return char.ToUpper(s[0]) + s[1..];
+        }
+    }
+}
