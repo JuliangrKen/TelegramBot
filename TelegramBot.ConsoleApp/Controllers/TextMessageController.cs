40159a61e6a66c10f09a7e16715521a349620972
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-18 11:19:04 +0300

    Добавил обработчик команды /start

--- TelegramBot.ConsoleApp/Controllers/TextMessageController.cs
+++ TelegramBot.ConsoleApp/Controllers/TextMessageController.cs
@@ -1,5 +1,7 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBot.ConsoleApp.Controllers
 {
@@ -11,8 +13,29 @@ namespace TelegramBot.ConsoleApp.Controllers
 
         public override async Task Handle(Message message, CancellationToken ct)
         {
-            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
-            await telegramBotClient.SendTextMessageAsync(message.Chat.Id, $"Получено текстовое сообщение", cancellationToken: ct);
+            switch (message.Text)
+            {
+                case "/start":
+
+                    // Объект, представляющий кноки
+                    var buttons = new List<InlineKeyboardButton[]>
+                    {
+                        new[]
+                        {
+                            InlineKeyboardButton.WithCallbackData($" Русский" , $"ru"),
+                            InlineKeyboardButton.WithCallbackData($" English" , $"en")
+                        }
+                    };
+
+                    // передаем кнопки вместе с сообщением (параметр ReplyMarkup)
+                    await telegramBotClient.SendTextMessageAsync(message.Chat.Id, $"<b>  Наш бот превращает аудио в текст.</b> {Environment.NewLine}" +
+                        $"{Environment.NewLine}Можно записать сообщение и переслать другу, если лень печатать.{Environment.NewLine}", cancellationToken: ct, parseMode: ParseMode.Html, replyMarkup: new InlineKeyboardMarkup(buttons));
+
+                    break;
+                default:
+                    await telegramBotClient.SendTextMessageAsync(message.Chat.Id, "Отправьте аудио для превращения в текст.", cancellationToken: ct);
+                    break;
+            }
         }
     }
 }
